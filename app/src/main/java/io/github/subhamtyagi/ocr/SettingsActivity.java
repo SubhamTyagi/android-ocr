ff1ebf870ccbe7058003a2f0b8c90f3b5b5a8c9b
Davide Panaroni <thepana00@gmail.com>
2023-02-16 15:17:05 +0100

    There is now only one way to pick languages
    There is no reason to be restricted to one language, so the option has been removed to simplify the preference code.

--- app/src/main/java/io/github/subhamtyagi/ocr/SettingsActivity.java
+++ app/src/main/java/io/github/subhamtyagi/ocr/SettingsActivity.java
@@ -9,12 +9,7 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreference;
 
 
-/**
- * Settings for App: I think all codes are self explanatory
- */
 public class SettingsActivity extends AppCompatActivity {
-
-    private static final String TAG = "SettingActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,37 +19,12 @@ public class SettingsActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
-        /*ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }*/
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
-
-            SwitchPreference enableMultipleLang = findPreference(getString(R.string.key_enable_multiple_lang));
-            ListPreference listPreference = findPreference(getString(R.string.key_language_for_tesseract));
-            MultiSelectListPreference multiSelectListPreference = findPreference(getString(R.string.key_language_for_tesseract_multi));
-
-            if (enableMultipleLang.isChecked()) {
-                multiSelectListPreference.setVisible(true);
-                listPreference.setVisible(false);
-            } else {
-                multiSelectListPreference.setVisible(false);
-                listPreference.setVisible(true);
-            }
-
-            enableMultipleLang.setOnPreferenceChangeListener((preference, newValue) -> {
-                boolean b = (boolean) newValue;
-                multiSelectListPreference.setVisible(b);
-                listPreference.setVisible(!b);
-
-                return true;
-            });
-
         }
 
 

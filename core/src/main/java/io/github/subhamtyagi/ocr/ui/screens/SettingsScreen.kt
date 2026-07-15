cb8981dc06dfe5ef8a77fb1da2dfbac2336236bc
Shubham Tyagi <shubham2tyagi7@gmail.com>
2026-07-15 14:18:58 +0530

    WIP: some changes

--- core/src/main/java/io/github/subhamtyagi/ocr/ui/screens/SettingsScreen.kt
+++ core/src/main/java/io/github/subhamtyagi/ocr/ui/screens/SettingsScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import io.github.subhamtyagi.ocr.R

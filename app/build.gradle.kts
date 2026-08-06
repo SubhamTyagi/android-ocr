d9547a9a19af84ca600915317daec8de63f5eb66
Shubham Tyagi <shubham2tyagi7@gmail.com>
2026-08-06 14:23:41 +0530

    new version release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.github.subhamtyagi.ocr"
         minSdk = 23
         targetSdk = 37
-        versionCode = 60
-        versionName = "6.0"
+        versionCode = 61
+        versionName = "6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
